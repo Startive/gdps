ced00249ac65ea460afd1c513ba379bc1b64573e
Startive <july2009chris@gmail.com>
2024-05-26 18:22:08 +0100

    win32 impl coming together

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set INCLUDE=C:\vcpkg\installed\x64-windows\include
-set SOURCE=src\*
-set LIBS=ucrt.lib kernel32.lib
+set SOURCE=src\win32.c
+set LIBS=kernel32.lib wsock32.lib ucrt.lib vcruntime.lib
 
 vcvars64.bat && ^
 cl /Fe:gdps.exe /Fo:obj\ /I%include% %SOURCE% /link /ENTRY:main %LIBS%
--- src/win32.c
+++ src/win32.c
@@ -1,10 +1,55 @@
 // WIN32 IMPLEMENTATION
+// or maybe just do a bunch of #ifdefs for linux and windows
+// but ill probably make another file that is just for unix so its cleaner
 
 #include <stdio.h>
-#include <Windows.h>
+#include <memory.h>
+#include <stdarg.h>
+#include <WinSock2.h>
+
+#define PORT 5555
+
+void CleanExit(char errmsg[], int errcode) {
+    printf("Error msg: %s\nError code: %i\n", errmsg, errcode);
+
+    WSACleanup();
+    exit(1);
+}
 
 int main(int argc, char **argv) {
-    SOCKET sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
+    WSADATA wsadata; ZeroMemory(&wsadata, sizeof(WSADATA));
+    int result = 0;
+    int opt = 1; // idk what this does???
+
+    // win32 stuff
+    result = WSAStartup(MAKEWORD(2,2), &wsadata);
+    if (result != 0) 
+        CleanExit("Error with WSAStartup.", result);
+
+    // Create socket
+    SOCKET sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sockfd == INVALID_SOCKET)
+        CleanExit("Error setting up the socket.", WSAGetLastError());
+
+    // Set sock opt, so we can 'resuse' the address and port
+    result = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt));
+    if (result != 0)
+        CleanExit("Failed to set sock opt.", WSAGetLastError());
+
+    // Bind
+    struct sockaddr_in addr; ZeroMemory(&addr, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+    result = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+    if (result != 0)
+        CleanExit("Binding has failed.", WSAGetLastError());
+
+    // Listen
+    result = listen(sockfd, SOMAXCONN);
+    if (result != 0)
+        CleanExit("Failed to listen on port 5555.", WSAGetLastError());
 
     return 0;
 }
